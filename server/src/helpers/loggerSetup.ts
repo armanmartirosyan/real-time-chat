1e9e913749f0c3c0152d6024809f7ced93b82a94
armanmartirosyan <armanmartirosyan81@gmail.com>
2024-10-02 17:10:49 +0400

    Added: morgan logger setup

---
+++ server/src/helpers/loggerSetup.ts
@@ -0,0 +1,27 @@
+import fs from "node:fs";
+import path from "node:path";
+import logger from "morgan";
+import { Express } from "express";
+import colors from "./colors.js";
+
+
+
+export function loggerSetup(app: Express, env: string): void {
+	try {
+		if (!(env === "development" || env === "production"))
+				throw new Error("configure the NODE_ENV variable.")
+		const folderName: string = path.join(__dirname, "..", "logs");
+		const fileName: string = path.join(folderName, `${env}.log`);
+		if (!fs.existsSync(folderName)) {
+			fs.mkdirSync(folderName);
+			console.log(`${colors.cyan}Directory for storing logs created at ${folderName}${colors.reset}`);
+		}
+		app.use(logger("common", {
+			stream: fs.createWriteStream(fileName, { flags: 'a' })
+		}));
+	}
+	catch(error: any) {
+		console.error(colors.red, error, colors.reset);
+		process.exit(1);
+	}
+}
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,20 +1,17 @@
 import dotenv from "dotenv";
 import mongoose from "mongoose";
+import colors from "./helpers/colors.js";
+import { loggerSetup } from "./helpers/loggerSetup.js";
 import express, {Express, Request, Response} from "express";
+
 
 dotenv.config();
 
 const app: Express = express();
 const PORT: number = Number(process.env.PORT) || 8080;
 
-mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING!)
-.then(() => {
-	console.log("MongoDB connected successfully");
-})
-.catch((error) => {
-	console.error('MongoDB connection error:', error);
-	process.exit(1);
-});
+loggerSetup(app, process.env.NODE_ENV!);
+
 
 
 app.use(express.json());
@@ -23,6 +20,24 @@ app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Server something else" });
 });
 
-app.listen(PORT, () => {
-	console.log(`Server is running on port ${PORT}`);
-});
+async function startServer(): Promise<void> {
+	try {
+		await mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING!)
+		.then(() => {
+			console.log(`${colors.cyan}MongoDB connected successfully${colors.reset}`);
+		})
+		.catch((error) => {
+			console.error("MongoDB connection error:", error);
+			process.exit(1);
+		});
+	
+		app.listen(PORT, () => {
+			console.log(`${colors.yellow}Server is running on port ${PORT}...${colors.reset}`);
+		});
+	} catch(error) {
+		console.error(colors.red, error, colors.reset);
+		process.exit(1);
+	}
+}
+
+startServer();
