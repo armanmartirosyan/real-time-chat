d9d9943b179a642dec198fc6dbcf2177f012c3ea
armanmartirosyan <armanmartirosyan81@gmail.com>
2024-10-02 15:30:07 +0400

    Added: mongoose setup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,23 @@
+import dotenv from "dotenv";
+import mongoose from "mongoose";
 import express, {Express, Request, Response} from "express";
 
+dotenv.config();
+
 const app: Express = express();
-const PORT: number = 8080;
+const PORT: number = Number(process.env.PORT) || 8080;
+
+mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING!)
+.then(() => {
+	console.log("MongoDB connected successfully");
+})
+.catch((error) => {
+	console.error('MongoDB connection error:', error);
+	process.exit(1);
+});
+
+
+app.use(express.json());
 
 app.get("/", (req: Request, res: Response) => {
 	res.json({ message: "Server something else" });
