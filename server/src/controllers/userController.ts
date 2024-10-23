5e7d4be8af3e011aeb562ac63967900618b0e06a
armanmartirosyan <armanmartirosyan81@gmail.com>
2024-10-23 14:27:55 +0400

    Modified: server validation

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,8 +1,10 @@
-import { Request, Response, NextFunction } from "express";
+import mongoose from "mongoose";
 import User from "../models/UserModel.js";
 import UserService from "../services/userService.js";
 import { userNamespace } from "../@types/index.d.js";
-import mongoose from "mongoose";
+import { Request, Response, NextFunction } from "express";
+import { Result, ValidationError, validationResult } from "express-validator";
+import APIError from "../exceptions/apiError.js";
 
 class UserController {
 	userService: UserService;
@@ -13,6 +15,9 @@ class UserController {
 
 	async registration(req: Request, res: Response, next: NextFunction): Promise<void> {
 		try {
+			const errors: Result<ValidationError> = validationResult(req);
+			if (!errors.isEmpty())
+				throw APIError.BadRequest("Validaiton Error", errors.array());
 			const {email, username, password, passwordConfirm }: userNamespace.UserCredentials = req.body;
 			const userData: userNamespace.IUserDTO = await this.userService.registration(email, username, password, passwordConfirm);
 			res.cookie("refreshToken", userData.tokenPair.refreshToken, {maxAge: 60 * 60 * 1000, httpOnly: true});
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,9 +1,18 @@
 import { Router, Request, Response  } from "express";
 import UserController from "../controllers/userController.js";
+import { body } from "express-validator";
 
 const userRoutes: Router = Router();
 const userController = new UserController();
 
-userRoutes.post("/registration", userController.registration.bind(userController));
+userRoutes.post("/registration",
+	body("email").isEmail(),
+	body("username").isLength({ min: 3, max: 25 }),
+	body("password").isLength({ min: 3, max: 64 }),
+	body("passwordConfirm").isLength({ min: 3, max: 64 }),
+	userController.registration.bind(userController)
+);
+
+
 
 export default userRoutes;
