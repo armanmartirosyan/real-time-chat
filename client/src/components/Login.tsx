b15d0d1d17faf4d9eb1e7c50e2eecd3edbd26cf3
Arman Martirosyan <arman.martirosyan@nrgaming.com>
2025-06-12 17:37:24 +0400

    Added: error validation/handling

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,6 +1,7 @@
 import "../@types/index.d";
 import React, { ChangeEvent, useContext, useState } from "react";
 import { UserContext, UserContextType } from "../contexts/userContext";
+import { ApiResponse, IUserCredentials } from "../@types/index.d";
 
 interface LoginProps {
 	isLoginPage: boolean;
@@ -10,15 +11,72 @@ interface LoginProps {
 export default function Login({ isLoginPage, setIsLoginPage }: LoginProps): React.JSX.Element {
 	const [email, setEmail] = useState<string>('');
 	const [password, setPassword] = useState<string>('');
+	const [isSubmitting, setIsSubmitting] = useState<boolean>(false);
+	const [generalError, setGeneralError] = useState<string>("");
+	const [errors, setErrors] = useState<IUserCredentials>({
+		email: "",
+		password: "",
+		username: "",
+		passwordConfirm: "",
+	});
 	const userContext: UserContextType = useContext<UserContextType>(UserContext);
 
 	function handleInputChange(e: React.ChangeEvent<HTMLInputElement>, handler: Function): void {
+		if (generalError) {
+			setGeneralError("");
+		}
+		setErrors((prev: IUserCredentials): IUserCredentials => ({
+			...prev,
+			[e.target.name]: "",
+		}));
 		handler(e.target.value);
+	}
+
+	function validateForm(): boolean {
+		let isValid: boolean = true;
+		const newErrors = { ...errors };
+		const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+		if (!emailRegex.test(email)) {
+			newErrors.email = "Please enter a valid email address";
+			isValid = false;
+		}
+		if (password.length < 8) {
+			newErrors.password = "Password must be at least 8 characters"
+			isValid = false;
+		}
+		if (!isValid) {
+			setEmail("");
+			setPassword("");
+		}
+		setErrors(newErrors);
+		return isValid
 	}
 
 	async function handleSubmit(e: React.FormEvent<HTMLFormElement>): Promise<void> {
 		e.preventDefault();
-		await userContext.login(email, password);
+		setGeneralError("");
+
+		if (!validateForm()) {
+			return;
+		}
+		try {
+			setIsSubmitting(true);
+			const response: ApiResponse = await userContext.login(email, password);
+
+			if (response.errors && response.errors.length > 0) {
+				setGeneralError(response.errors[0].message || "Registration failed. Please try again later.");
+			}
+		} catch (error: any) {
+			console.error(error);
+			setGeneralError("An error occurred. Please try again later.")
+		} finally {
+			setIsSubmitting(false);
+			if (!generalError) {
+				setEmail("");
+				setPassword("");
+			}
+		}
 	}
 
 	function setAccount(e: React.MouseEvent<HTMLAnchorElement>): void {
@@ -26,11 +84,19 @@ export default function Login({ isLoginPage, setIsLoginPage }: LoginProps): Reac
 		setIsLoginPage(!isLoginPage);
 	}
 
+	function getErrorMessage(field: string): string {
+		if (errors[field]) {
+			return errors[field];
+		}
+		return "";
+	}
+
 	return (
 		<section>
 			<div className="signin">
 				<div className="content">
 					<h2>Sign In</h2>
+					{generalError && <div className="general-error">{generalError}</div>}
 					<form className="root-form" onSubmit={handleSubmit}>
 						<div className="input-box">
 							<input
@@ -41,6 +107,7 @@ export default function Login({ isLoginPage, setIsLoginPage }: LoginProps): Reac
 								placeholder=" "
 								autoComplete="email"
 							/>
+							{getErrorMessage("email") && <p className="error-message">{getErrorMessage("email")}</p>}
 							<label>Email</label>
 						</div>
 						<div className="input-box">
@@ -52,6 +119,7 @@ export default function Login({ isLoginPage, setIsLoginPage }: LoginProps): Reac
 								placeholder=" "
 								autoComplete="current-password"
 							/>
+							{getErrorMessage("password") && <p className="error-message">{getErrorMessage("password")}</p>}
 							<label>Password</label>
 						</div>
 						<div className="links">
