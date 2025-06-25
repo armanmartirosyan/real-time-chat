5c2f946ddfda3603dcd133ee88409a61547f115b
Arman Martirosyan <arman.martirosyan@nrgaming.com>
2025-06-25 15:39:09 +0400

    Updated: readFile error handle

--- server/src/services/tokenService.ts
+++ server/src/services/tokenService.ts
@@ -1,61 +1,74 @@
 import fs from "node:fs";
 import path from "node:path";
 import mongoose from "mongoose";
-import Tokens, {ITokens } from "../models/TokenModel";
-import jwt, {JwtPayload} from "jsonwebtoken";
+import Tokens, { ITokens } from "../models/TokenModel";
+import jwt, { JwtPayload } from "jsonwebtoken";
 import { JwtTokens } from "../@types/index.d";
 import APIError from "../exceptions/apiError";
+import colors from "../helpers/colors";
 
 class TokenService {
   private privateKey: string;
   private publicKey: string;
 
   constructor() {
-    this.privateKey = fs.readFileSync(path.join(__dirname, "../certificates/private.pem"), 'utf-8');
-    this.publicKey = fs.readFileSync(path.join(__dirname, "../certificates/public.pem"), 'utf-8');
+    try {
+      this.privateKey = fs.readFileSync(path.join(__dirname, "../certificates/private.pem"), 'utf-8');
+      console.log(`${colors.cyan}Private key loaded successfully.${colors.reset}`);
+
+      this.publicKey = fs.readFileSync(path.join(__dirname, "../certificates/public.pem"), 'utf-8');
+      console.log(`${colors.cyan}Public key loaded successfully.${colors.reset}`);
+    } catch (error: any) {
+      console.error('\n--- CRITICAL ERROR: Failed to load cryptographic certificates ---');
+      console.error(`An unexpected error occurred while loading certificates: ${error.message}`);
+      console.error('Details:', error);
+      console.error('------------------------------------------------------------------\n');
+
+      process.exit(1);
+    }
   }
 
-	generateTokens(payload: JwtPayload): JwtTokens.TokenPair {
+  generateTokens(payload: JwtPayload): JwtTokens.TokenPair {
     const accessToken: string = jwt.sign(payload, this.privateKey, { algorithm: 'RS256', expiresIn: '1h' });
     const refreshToken: string = jwt.sign(payload, this.privateKey, { algorithm: 'RS256', expiresIn: '1d' });
-		
-		return {accessToken, refreshToken};
-	}
 
-	async saveToken(userID: mongoose.Schema.Types.ObjectId, refreshToken: string): Promise<void> {
-		const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
+    return { accessToken, refreshToken };
+  }
 
-		await Tokens.findOneAndUpdate(
-			{ userID },
-			{ refreshToken, expiresAt},
-			{ upsert: true, new: true }
-		);
-		return ;
-	}
+  async saveToken(userID: mongoose.Schema.Types.ObjectId, refreshToken: string): Promise<void> {
+    const expiresAt = new Date(Date.now() + 24 * 60 * 60 * 1000);
 
-	async findToken(refreshToken: string): Promise<ITokens | null> {
-		const token: ITokens | null = await Tokens.findOne({ refreshToken });
-		return token;
-	}
+    await Tokens.findOneAndUpdate(
+      { userID },
+      { refreshToken, expiresAt },
+      { upsert: true, new: true }
+    );
+    return;
+  }
 
-	async removeToken(refreshToken: string): Promise<void> {
-		const token: ITokens | null = await Tokens.findOne({ refreshToken });
-		if (!token)
-			throw APIError.NoContent("No Content");
-		await token.deleteOne();
-		return ;
-	}
+  async findToken(refreshToken: string): Promise<ITokens | null> {
+    const token: ITokens | null = await Tokens.findOne({ refreshToken });
+    return token;
+  }
 
-	verifyToken(token: string): JwtTokens.VerifiedJWT {
-		try {
-			const userData: JwtPayload | string = jwt.verify(token, this.publicKey, { algorithms: ['RS256'] });
-			if (typeof userData === "string")
-				throw APIError.UnauthorizedError();
-			return userData as JwtTokens.VerifiedJWT;
-		} catch(error: any) {
-			throw APIError.UnauthorizedError();
-		}
-	}
+  async removeToken(refreshToken: string): Promise<void> {
+    const token: ITokens | null = await Tokens.findOne({ refreshToken });
+    if (!token)
+      throw APIError.NoContent("No Content");
+    await token.deleteOne();
+    return;
+  }
+
+  verifyToken(token: string): JwtTokens.VerifiedJWT {
+    try {
+      const userData: JwtPayload | string = jwt.verify(token, this.publicKey, { algorithms: ['RS256'] });
+      if (typeof userData === "string")
+        throw APIError.UnauthorizedError();
+      return userData as JwtTokens.VerifiedJWT;
+    } catch (error: any) {
+      throw APIError.UnauthorizedError();
+    }
+  }
 }
 
 export default TokenService;
