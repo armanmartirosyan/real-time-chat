95d2a08c9997fe0ce47a3705048f0ddf7961540f
Arman Martirosyan <arman.martirosyan@nrgaming.com>
2025-06-30 14:43:15 +0400

    Clean-up: whitespaces

--- server/src/models/TokenModel.ts
+++ server/src/models/TokenModel.ts
@@ -1,28 +1,28 @@
-import mongoose, {Document, Schema, Model} from "mongoose";
+import mongoose, { Document, Schema, Model } from "mongoose";
 
 export interface ITokens extends Document {
-	_id: mongoose.Schema.Types.ObjectId;
-	userID: mongoose.Schema.Types.ObjectId,
-	refreshToken: string,
-	expiresAt: Date,
+  _id: mongoose.Schema.Types.ObjectId;
+  userID: mongoose.Schema.Types.ObjectId,
+  refreshToken: string,
+  expiresAt: Date,
 }
 
 const tokensSchema: Schema<ITokens> = new Schema({
-    userID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
-        required: true
-    },
-    refreshToken: {
-        type: String,
-        required: true,
-        unique: true
-    },
-	expiresAt: {
-        type: Date,
-        default: () => new Date(Date.now() + 24 * 60 * 60 * 1000),
-        expires: '1d'
-    },
+  userID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  refreshToken: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  expiresAt: {
+    type: Date,
+    default: () => new Date(Date.now() + 24 * 60 * 60 * 1000),
+    expires: '1d'
+  },
 }, { timestamps: true });
 
 
